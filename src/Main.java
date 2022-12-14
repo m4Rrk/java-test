38d716a41e15b10d8976b0a5329e22919ef3620f
Mark Varakuta <markvarakuta3@gmail.com>
2022-12-14 17:59:37 +0200

    Added new lines of code

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println(3-2);
+        System.out.println("Hi");
+        for(int i = 1; i<=10;i++){
+            System.out.println(i);
+        }
     }
 }
