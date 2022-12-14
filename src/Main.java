e6c4d92b3198cfd64580ee295a67769269ace81c
Mark Varakuta <markvarakuta3@gmail.com>
2022-12-14 18:01:25 +0200

    removed 1 line of code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println(3-2);
+        
         System.out.println("Hi");
         for(int i = 1; i<=10;i++){
             System.out.println(i);
